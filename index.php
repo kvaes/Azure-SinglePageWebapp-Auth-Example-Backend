dac6155d5efca130309c4b45aa4fb96c1c984238
Karim Vaes <karim@kvaes.be>
2017-03-12 20:14:43 +0100

    check if accesstoken was provided

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?PHP
-
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Emarref\Jwt\Claim;
+$jwt = new Emarref\Jwt\Jwt();
 
 $headers = getallheaders();
 $authorization = explode(' ', $headers['Authorization']);
 $accessToken = $authorization[1];
 
-use Emarref\Jwt\Claim;
-$jwt = new Emarref\Jwt\Jwt();
-$token = $jwt->deserialize($accessToken);
-print_r($token);
+if ($accessToken == "") {
+	echo "No access token retrieved.";
+} else {
+	$token = $jwt->deserialize($accessToken);
+	print_r($token);
+}
 
 ?>

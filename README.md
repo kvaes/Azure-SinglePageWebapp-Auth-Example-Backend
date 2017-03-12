a89245b70fc4fd3336e2dbc3ac3c4caf1b1fedab
Karim Vaes <karim@kvaes.be>
2017-03-12 18:47:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Azure-SinglePageWebapp-Auth-Example-Backend
+
+Usage
+=====
+Deploy onto a webserver. Adjust index file or set the environment variables. Enjoy!
+
+Dependency
+==========
+This repository contains the code for the backend-end. The frontend repository can be found here ; https://github.com/kvaes/Azure-SinglePageWebapp-Auth-Example
+
+Configuration
+=============
+* index.php : Change the settings (app id, app secret redirect url), or set them as environment variables in your webapp.
+
+Credits
+=======
+Based on [thenetworg/oauth2-azure](https://github.com/thenetworg/oauth2-azure)
---
+++ index.php
@@ -0,0 +1,33 @@
+<?PHP
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$provider = new TheNetworg\OAuth2\Client\Provider\Azure([
+    'clientId'          => getenv('clientId');,
+    'clientSecret'      => getenv('clientSecret');,
+    'redirectUri'       => getenv('redirectUri');
+]);
+
+$headers = getallheaders();
+
+print_r($headers);
+
+$authorization = explode(' ', $headers['Authorization']);
+$accessToken = $authorization[1];
+
+try {
+    $claims = $provider->validateAccessToken($accessToken);
+} catch (Exception $e) {
+    echo "Something went wrong :-(";
+}
+
+$graphAccessToken = $provider->getAccessToken('jwt_bearer', [
+    'resource' => 'https://graph.microsoft.com/v1.0/',
+    'assertion' => $accessToken,
+    'requested_token_use' => 'on_behalf_of'
+]);
+
+$me = $provider->get('https://graph.microsoft.com/v1.0/me', $graphAccessToken);
+print_r($me);
+
+?>
